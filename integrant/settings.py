7cc792123f173eaae3988e29cfb6a1e75b3b031f
MahmoudMansourr <118729159+MahmoudMansourr@users.noreply.github.com>
2023-05-05 19:37:58 +0300

    Update settings.py

--- integrant/settings.py
+++ integrant/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'companyDB',
-    'USER': 'mansour',
-    'PASSWORD': '123',
+    'USER': '',
+    'PASSWORD': '',
     'HOST': 'localhost',
     'PORT': '3306',
 }
